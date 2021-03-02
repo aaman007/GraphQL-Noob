b82c7bf7927df0ef60a4eb31bc466c769cfc0d28
Aaman007 <aaman.sunny007@gmail.com>
2021-03-02 13:46:51 +0600

    GraphQL queries added

--- quiz/schema.py
+++ quiz/schema.py
@@ -31,9 +31,17 @@ class OptionType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
+    # List
     categories = DjangoListField(CategoryType)
     quizzes = DjangoListField(QuizType)
     questions = DjangoListField(QuestionType)
+    options = DjangoListField(OptionType)
+    question_options = graphene.List(OptionType, id=graphene.Int())
+
+    # Single Objects
+    quiz = graphene.Field(QuizType, id=graphene.Int())
+    question = graphene.Field(QuestionType, id=graphene.Int())
+    option = graphene.Field(OptionType, id=graphene.Int())
 
     @staticmethod
     def resolve_categories(root, info):
@@ -44,8 +52,28 @@ class Query(graphene.ObjectType):
         return Quiz.objects.all()
 
     @staticmethod
+    def resolve_quiz(root, info, id):
+        return Quiz.objects.get(pk=id)
+
+    @staticmethod
     def resolve_questions(root, info):
         return Question.objects.all()
+
+    @staticmethod
+    def resolve_question(root, info, id):
+        return Question.objects.get(pk=id)
+
+    @staticmethod
+    def resolve_options(root, info):
+        return Option.objects.all()
+
+    @staticmethod
+    def resolve_question_options(root, info, id):
+        return Option.objects.filter(question_id=id)
+
+    @staticmethod
+    def resolve_option(root, info, id):
+        return Option.objects.get(pk=id)
 
 
 schema = graphene.Schema(query=Query)
@@ -77,5 +105,29 @@ query{
 }
 
 Query2:
+query{
+  quiz(id:2){
+    title
+  }
+}
 
+Query3:
+query{
+  question(id:1){
+    title
+  }
+  questionOptions(id:1){
+    answer
+  }
+}
+
+Query4:
+query GetQuestion($id: Int = 2){
+  question(id:$id){
+    title
+  }
+  questionOptions(id:$id){
+    answer
+  }
+}
 """
